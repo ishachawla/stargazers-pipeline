74512ce3663193ca6bef916f83d3c0b34a95103c
Isha Chawla <chawla.i@husky.neu.edu>
2026-03-22 22:16:20 +0000

    add requirements.txt

--- test_fetch_results.py
+++ test_fetch_results.py
@@ -2,10 +2,9 @@ import duckdb
 
 conn = duckdb.connect('stargazers.duckdb')
 
-print("=== TABLES ===")
-print(conn.execute("SHOW ALL TABLES").df())
 
-print("\n=== COUNTS PER REPO ===")
+
+print("\n=== COUNTS PER REPO BY RAW DATA SOURCE ===")
 print(conn.execute("""
     SELECT repo_name, COUNT(*) as total_stargazers
     FROM github_raw.stargazers
@@ -13,7 +12,7 @@ print(conn.execute("""
     ORDER BY total_stargazers DESC
 """).df())
 
-print("\n=== SAMPLE ROWS ===")
+print("\n=== SAMPLE ROWS IN RAW DATA SOURCE ===")
 print(conn.execute("""
     SELECT * FROM github_raw.stargazers
     LIMIT 5
